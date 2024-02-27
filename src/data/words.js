e80d7e3f5515039ad5d23307a7a049d391799e62
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-02-27 17:07:13 -0300

    Configurações iniciais do projeto

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordList = {
+    carro:["Motor","Capô","Porta","Antena","Pneu","Parachoque"],
+    fruta:["Banana","Maçã","Pera","Mamão","Laranja","Abacaxi","Limão"],
+    corpo:["Braço","Perna","Cérebro","Pescoço","Olhos","Pé","Coração"],
+    computador:["Mouse","Teclado","Monitor","Gabinete"],
+    programacao:["Linguagem","Framework","Javascript","React","Blazor","Java"],
+    alimento:["Arroz","Feijão","Carne","Leite","Ovos","Macarrão","Farinha"]
+};
