efdeb657e2c7bb5d53d97b8a4c7d893946eacea9
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-02-28 16:16:40 -0300

    Alternando os estágios do jogo

--- src/App.js
+++ src/App.js
@@ -22,13 +22,28 @@ function App() {
   const [gameState, setGameState] = useState(stages[0].name);
   const [words] = useState(wordList);
 
+  //Start the secret words game
+  const startGame = () =>{
+    setGameState(stages[1].name);
+  }
+
+  //Process the letter input
+  const verifyLetter = () =>{
+    setGameState(stages[2].name);
+  }
+
+  //Restats the game
+  const retry = () =>{
+    setGameState(stages[0].name);
+  }
+
   console.log(words)
 
   return (
     <div className="App">
-      {gameState === "start" && <StartScreen />}
-      {gameState === "game" && <Game />}
-      {gameState === "end" && <GameOver/>}
+      {gameState === "start" && <StartScreen startGame = {startGame} />}
+      {gameState === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameState === "end" && <GameOver retry={retry}/>}
     </div>
   );
 }
