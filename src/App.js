90b293370da72cd7d39aa5cee72b712f9be56857
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-02-28 11:07:06 -0300

    Criando componentes do projeto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
-import StartScreen from './components/StartScreen';
+//CSS
+import "./App.css";
+
+//React
+import { useCallback, useEffect, useState } from "react";
+
+//Data
+import { wordList } from "./data/words";
+
+//Components
+import StartScreen from "./components/StartScreen";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameState, setGameState] = useState(stages[0].name);
+  const [words] = useState(wordList);
+
+  console.log(words)
+
   return (
     <div className="App">
-     <StartScreen/>
+      {gameState === "start" && <StartScreen />}
+      {gameState === "game" && <Game />}
+      {gameState === "end" && <GameOver/>}
     </div>
   );
 }
